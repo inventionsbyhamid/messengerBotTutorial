77d283ce30a836c0fe14251cb9f9b6d07181a952
Hamid Siddiqui <inventionsbyhamid@gmail.com>
2016-04-15 12:40:37 +0530

    update route for tutorial ease

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
    
-  match '/webook', to: 'webhooks#index', via: [:get, :post]
+  match '/', to: 'webhooks#index', via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
